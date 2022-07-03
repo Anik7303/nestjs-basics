536a5a401b69dcbe7c8787b8e073b8393f967df0
Anik Mohammad <anik7303@gmail.com>
2022-07-04 02:01:03 +0600

    serialized data

---
+++ src/interceptors/serializer.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializerInterceptor(dto));
+}
+
+export class SerializerInterceptor implements NestInterceptor {
+  constructor(private dto: ClassConstructor) {}
+
+  intercept(_context: ExecutionContext, handler: CallHandler): Observable<any> {
+    return handler.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
