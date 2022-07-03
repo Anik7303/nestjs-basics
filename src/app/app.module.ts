7c29bdc707bcca91ea8351187e10033cac981d39
Anik Mohammad <anik7303@gmail.com>
2022-07-03 15:20:46 +0600

    configuring typeorm and added user & report entities

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule, User } from 'src/users';
+import { ReportsModule, Report } from 'src/reports';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/reports/index.ts
@@ -0,0 +1,4 @@
+export * from './report.entity';
+export * from './reports.controller';
+export * from './reports.module';
+export * from './reports.service';
